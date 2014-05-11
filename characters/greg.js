0507f49dd2b5799f9269b75a750aac39fb953e4b
Jason Orendorff <jason.orendorff@gmail.com>
2014-05-11 03:17:44 -0500

    add pictures for greg and steve, not that the server can use them

--- characters/greg.js
+++ characters/greg.js
@@ -1,4 +1,5 @@
 // name: Good Guy Greg
+// picture: http://i1.kym-cdn.com/photos/images/newsfeed/000/288/060/bc5.jpg
 
 function* main() {
     while (true) {
--- characters/steve.js
+++ characters/steve.js
@@ -1,4 +1,5 @@
 // name: Scumbag Steve
+// picture: http://i1.kym-cdn.com/photos/images/original/000/093/965/IMG_0575.JPG
 
 function* main() {
     while (true) {
