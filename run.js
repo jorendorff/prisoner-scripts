c9864d836d99fc0ff63f4e9fc3fb5192d823a1f5
Jason Orendorff <jason.orendorff@gmail.com>
2014-05-11 13:47:13 -0500

    Run a test match between Steve and Greg. Works!

--- run.js
+++ run.js
@@ -18,7 +18,6 @@ var robots = {};
 // Connect to the server and play forever using the given robot object.
 function start_robot(robot) {
     var username = robot.metadata.username;
-    console.log("starting robot", username);
 
     // Create the main generator function for this robot.
     var main = compile(robot.code);
@@ -32,12 +31,13 @@ function start_robot(robot) {
     return new Promise(function (resolve, reject) {
         // I can't find any API in socket.io-client for being called back on
         // errors, so `reject` isn't used.
-        console.log("connecting to:", url.format(connect_url));
+        console.log(username, "connecting to", url.format(connect_url));
         var socket = io.connect(url.format(connect_url), {"force new connection": true});
 
         var games = {};
 
         function compute_move(game_id, opponent_previous_move) {
+            console.log(username, game_id, opponent_previous_move);
             var result = games[game_id].next(opponent_previous_move);
             if (result.done)
                 throw new Error(username + " returned instead of yielding a move in game " + game_id);
@@ -49,21 +49,23 @@ function start_robot(robot) {
 
         socket.on('connect', function () {
             console.log(username, "connect");
-            socket.emit('add:user', username);
             robot.socket = socket;
-            robots[robot.name] = robot;
-            resolve(robot);
+            socket.emit('add:user', username);
+            // The response to this will be a 'login' message.
         });
 
         socket.on('login', function (data) {
-            console.log("login:", data);
+            robot.id = data.user._id;
+            console.log(username, "login as", robot.id);
+            robots[robot.name] = robot;
+            resolve(robot);
         });
 
         socket.on('game:init', function (msg) {
             var game_id = msg.game_id;
 
             // Send the server our code.
-            socket.emit('game:ready', {game_id: game_id, code: code});
+            socket.emit('game:ready', {game_id: game_id, code: robot.code});
 
             // Call the generator function to create a generator object.
             games[game_id] = main();
@@ -137,11 +139,8 @@ function start_main() {
 }
 
 start_main().then(function () {
-    console.info("all started!");
-    if (robots.greg.socket === robots.steve.socket)
-        console.log("disaster");
-    else
-        console.log("success");
+    console.info("all started! starting a match:");
+    robots.steve.socket.emit("play:now", [robots.greg.id]);
 }).catch(function (exc) {
     console.error(exc);
 });
