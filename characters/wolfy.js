579d48ce986a6073334bbf3dc02b974a82143641
Jason Orendorff <jason.orendorff@gmail.com>
2014-05-12 08:45:02 -0500

    make insanity wolf crazier.

--- characters/wolfy.js
+++ characters/wolfy.js
@@ -2,13 +2,9 @@
 // picture: https://i.chzbgr.com/maxW500/7936340480/h79D42825/
 
 function* main() {
-    for (var i = 0; i < 80; i++) {
-        yield COOPERATE;
-    }
-
     // Favourite cocktail?
-    while (true) {
+    for (var i = 0; i < 100; i++) {
         // MOLOTOV
-        yield Math.random() > .5 ? COOPERATE : DEFECT;
+        yield Math.random() > i / 100 ? COOPERATE : DEFECT;
     }
 }
