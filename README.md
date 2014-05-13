9d0bf8fb65bcba701cacc4ae9ce9b46f65b6f84a
Jason Orendorff <jason.orendorff@gmail.com>
2014-05-13 13:09:59 -0500

    Migrate tests to mocha. They work great now. Update README.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # prisoner-scripts: Robot players for the Prisoner's Dilemma
 
-To run these:
+The script in this repository connects several robot players to the [prisoners
+server](https://github.com/egdelwonk/prisoners) on localhost. The robots
+happily play games whenever the server tells them to.
+
+To run the robots:
 
     $ node --harmony-generators run.js
 
-**You must use Node.js 0.11 or later** because this code uses generators. As of
-this writing, 0.11 is still pre-release. You can download it here:
+To run tests:
+
+    $ mocha --harmony-generators test.js
+
+**You must use Node.js 0.11 or later** because this code uses ES6 generators.
+As of this writing, 0.11 is still pre-release. You can download it here:
 http://nodejs.org/dist/v0.11.9/
--- test.js
+++ test.js
@@ -1,36 +1,14 @@
 'use strict';
 
 var robots = require('./robots');
+var assert = require('assert');
 
-function addControlsToSocket(socket, username) {
-    socket.on('observe:init', function (msg) {
-        var pendingID = msg.clientTag + "/" + msg.players[1];
-        console.info("<", username, "observe:init", msg.game_id, msg.clientTag,
-                     msg.players[0], msg.players[1]);
-        Game.started(msg);
-    });
-
-    socket.on('observe:progress', function (msg) {
-        console.info("<", username, "observe:progress", msg.game_id,
-                     "P1:" + msg.player1_move, "P2:" + msg.player2_move,
-                     "(" + msg.player1_score + "-" + msg.player2_score + ")");
-    });
-
-    socket.on('observe:over', function (msg) {
-        console.info("<", username, "observe:over", msg.game_id,
-                     "(" + msg.player1_score + "-" + msg.player2_score + ")");
-        Game.ended(msg);
-    });
-
-    socket.on('tournament:started', function (msg) {
-        console.info("<", username, "tournament:started", msg.clientTag);
-    });
-
-    socket.on('tournament:done', function (msg) {
-        console.info("<", username, "tournament:done", msg.clientTag);
-        tournaments[msg.clientTag]._resolve(msg.scores);
-    });
-}
+// ## Controls
+//
+// Before we can test the robots, we need a way to launch games.  The robots
+// themselves only play the games they're asked to play.  Here we add the
+// ability to start games and tournaments, and get the scores when they're
+// done.
 
 var nextClientTag = 0;
 var pendingGames = Object.create(null);
@@ -63,75 +41,206 @@ Game.ended = function (msg) {
     delete games[msg.game_id];
 };
 
-function playGame(name1, names) {
-    var p1 = robots.byName[name1];
-    var ids = names.map(function (name) { return robots.byName[name].id; });
+function addControlsToSocket(socket, username) {
+    socket.on('observe:init', function (msg) {
+        var pendingID = msg.clientTag + "/" + msg.players[1];
+        console.info("<", username, "observe:init", msg.game_id, msg.clientTag,
+                     msg.players[0], msg.players[1]);
+        Game.started(msg);
+    });
+
+    socket.on('observe:progress', function (msg) {
+        console.info("<", username, "observe:progress", msg.game_id,
+                     "P1:" + msg.player1_move, "P2:" + msg.player2_move,
+                     "(" + msg.player1_score + "-" + msg.player2_score + ")");
+    });
+
+    socket.on('observe:over', function (msg) {
+        console.info("<", username, "observe:over", msg.game_id,
+                     "(" + msg.player1_score + "-" + msg.player2_score + ")");
+        Game.ended(msg);
+    });
+
+    socket.on('tournament:started', function (msg) {
+        console.info("<", username, "tournament:started", msg.clientTag);
+    });
+
+    socket.on('tournament:done', function (msg) {
+        console.info("<", username, "tournament:done", msg.clientTag);
+        tournaments[msg.clientTag]._resolve(msg.scores);
+    });
+}
+
+function playGamesWith(robot, opponents) {
+    var ids = opponents.map(function (r) { return r.id; });
     var clientTag = nextClientTag++;
-    console.info(">", "robot-" + p1.name, "play:now", ids);
-    p1.socket.emit("play:now", ids, clientTag);
-    return names.map(function (name) {
-        var p2 = robots.byName[name];
-        return new Game(p1, p2, clientTag).done;
+    console.info(">", "robot-" + robot.name, "play:now", ids);
+    robot.socket.emit("play:now", ids, clientTag);
+    return opponents.map(function (opp) {
+        return new Game(robot, opp, clientTag).done;
     });
 }
 
-function playOneGame(name1, name2) {
-    return playGame(name1, [name2])[0];
+function playOneGame(r1, r2) {
+    return playGamesWith(r1, [r2])[0];
 }
 
-function test(name1, score1, name2, score2) {
-    var expected = name1 + " " + score1 + ", " + name2 + " " + score2;
-    return playOneGame(name1, name2).then(function (game) {
-        var scores = game.scores;
-        var actual = name1 + " " + scores[0] + ", " + name2 + " " + scores[1];
-        console.log((actual === expected ? "PASS" : "FAIL") + " - " + actual);
-        return actual === expected;
-    })
-}
-
-function tournament(names) {
-    var ids = names.map(function (name) {
-        if (!(name in robots.byName))
-            throw new Error("no robot named '" + name + "'");
-        return robots.byName[name].id;
-    });
+function tournament(players) {
+    var ids = players.map(function (r) { return r.id; });
     var clientTag = nextClientTag++;
 
     // We have to create a Game object for each game we expect the server to
     // start.  This means all pairs of ids:
     for (var i = 0; i < ids.length; i++) {
         for (var j = i + 1; j < ids.length; j++) {
-            new Game(robots.byName[names[i]], robots.byName[names[j]], clientTag);
+            new Game(players[i], players[j], clientTag);
         }
     }
 
-    robots.byName.greg.socket.emit('tournament:start', {players: ids, clientTag: clientTag});
+    players[0].socket.emit('tournament:start', {players: ids, clientTag: clientTag});
     return new Promise(function (resolve, reject) {
         tournaments[clientTag] = {_resolve: resolve, _reject: reject};
     });
 }
 
-robots.startAll().then(function () {
-    console.info("all started!");
 
-    for (var name in robots.byName) {
-        addControlsToSocket(robots.byName[name].socket, "robot-" + name);
+// ## Running games locally
+
+// It is also possible to run a game locally and sychronously (though this
+// doesn't support forfeiting or the `sleep` thing that characters/froggy.js
+// does).
+var COOPERATE = 'COOPERATE', DEFECT = 'DEFECT';
+var NROUNDS = 100;
+function playLocally(r0, r1) {
+    var g0 = r0.main(), g1 = r1.main();
+    var m0 = undefined, m1 = undefined;
+    var s0 = 0, s1 = 0;
+    for (var i = 0; i < NROUNDS; i++) {
+        var moves = [g0.next(m1), g1.next(m0)];
+        if (moves[0].done)
+            throw new Error("unexpected quit by " + r0.name);
+        if (moves[1].done)
+            throw new Error("unexpected quit by " + r1.name);
+        m0 = moves[0].value;
+        m1 = moves[1].value;
+        if (m0 !== COOPERATE && m0 !== DEFECT)
+            throw new Error("unexpected move by " + r0.name + ": " + m0);
+        if (m1 !== COOPERATE && m1 !== DEFECT)
+            throw new Error("unexpected move by " + r1.name + ": " + m1);
+        var scores = payouts[m0][m1];
+        s0 += scores[0];
+        s1 += scores[1];
+    }
+    return [s0, s1];
+}
+
+var payouts = {
+    COOPERATE: {
+        COOPERATE: [3, 3],
+        DEFECT: [0, 5],
+    },
+    DEFECT: {
+        COOPERATE: [5, 0],
+        DEFECT: [1, 1],
+    }
+};
+
+
+// ## Tests
+
+// Hack promise support into Mocha.
+function wrapForGenerators(f) {
+    return function () {
+        function *g() {}
+        var test = arguments[arguments.length - 1];
+        if (test.constructor === g.constructor) {
+            arguments[arguments.length - 1] = function (done) {
+                var gen = test();
+                function pump(value) {
+                    var result;
+                    try {
+                        result = gen.next(value);
+                    } catch (exc) {
+                        return done(exc, undefined);
+                    }
+
+                    if (result.done) {
+                        return done(undefined, result.value);
+                    } else {
+                        result.value.then(pump).catch(function (exc) {
+                            done(exc, undefined);
+                        });
+                    }
+                }
+                pump(undefined);
+            }
+        }
+        return f.apply(this, arguments);
+    };
+}
+
+it = wrapForGenerators(it);
+before = wrapForGenerators(before);
+
+describe('robots', function () {
+    var r = robots.byName;
+
+    before(function* () {
+        yield robots.startAll();
+        for (var name in robots.byName) {
+            addControlsToSocket(robots.byName[name].socket, "robot-" + name);
+        }
+    });
+
+    function match(combatants) {
+        var names = combatants.split("-");
+        it("should be able to match " + names[0] + " with " + names[1], function* () {
+            var a = r[names[0]], b = r[names[1]];
+            var game = yield playOneGame(a, b);
+            assert.deepEqual(game.scores, playLocally(a, b));
+        });
     }
 
-    var tests = [
-        test("steve", 500, "greg", 0),
-        //test("steve", 476, "froggy", 6),
-        test("greg", 300, "walter", 300),
-        test("steve", 104, "walter", 99)
-    ];
-    return Promise.all(tests);
-}).then(function () {
-    return tournament("steve greg walter skier cotton penguin".split(" "));
-}).then(function (scores) {
-    scores.forEach(function (pair) {
-        console.log(robots.byId[pair.user_id].name, pair.score);
+    match('greg-steve');
+    match('steve-greg');
+    match('steve-walter');
+    match('cotton-skier');
+    match('skier-walter');
+
+    it("should be able to run multiple matches at once", function* () {
+        // I choose walter because he is stateful; one of the things we're
+        // testing here is that the games are properly independent.
+        var me = r.walter;
+        var them = [r.cotton, r.greg, r.steve, r.skier];
+        var games = yield Promise.all(playGamesWith(me, them));
+        them.forEach(function (opp, i) {
+            assert.deepEqual(games[i].scores, playLocally(me, opp));
+        });
     });
-}).catch(function (exc) {
-    console.error(exc.stack);
-    console.error(exc);
-}).then(process.exit);
+
+    var PENGUIN_LIFE = 37
+
+    it("should handle the penguin case (doofy behavior by a player -> forfeit)", function* () {
+        var game = yield playOneGame(r.steve, r.penguin);
+        assert.deepEqual(game.scores, [5 * NROUNDS, 0]);
+
+        assert(NROUNDS > PENGUIN_LIFE); // this next test would be invalid otherwise
+        game = yield playOneGame(r.greg, r.penguin);
+        assert.deepEqual(game.scores, [
+            3 * PENGUIN_LIFE + 5 * (NROUNDS - PENGUIN_LIFE),
+            3 * PENGUIN_LIFE
+        ]);
+    });
+
+    it("should be able to run a tournament", function* () {
+        var players = [r.steve, r.greg, r.walter, r.skier, r.cotton, r.penguin];
+        var results = yield tournament(players);
+        console.log(results);
+        var expected = [1752, 1311, 1604, 1400, 1051, 429];
+        assert.strictEqual(players.length, results.length);
+        players.forEach(function (bot, i) {
+            assert.strictEqual(results[i].user_id, bot.id);
+            assert.strictEqual(results[i].score, expected[i]);
+        });
+    });
+});
